8259822df065189b27f67970b5692ad64e5de63f
Veronika Bartosova <veronika.bartosova@synapse.com>
2018-04-11 10:27:46 +0200

    added footer

--- js/script.js
+++ js/script.js
@@ -4,5 +4,10 @@ $(document).ready(function(){
   });
 });
 
+window.addEventListener("load", setHeight);
+window.addEventListener("resize", setHeight);
 
-	
+function setHeight() {
+  var video = document.querySelector(".yt");
+  video.style.height = video.offsetWidth / 1.77 + 'px';
+}
